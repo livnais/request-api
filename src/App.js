c86a3fed3c142a7d1b0abfbaddc5a326302a0a40
iota <iota>
2020-12-18 14:26:27 +0100

    yolo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import "antd/dist/antd.css";
 import moment from "moment";
-import { DatePicker, Space, Input, Card, Alert } from "antd";
+import { DatePicker, Space, Input, Card, Steps } from "antd";
 
 const { Search } = Input;
+const { Step } = Steps;
 
 const URLWSHOTEL = "https://wshotelapp.herokuapp.com/v1/booking";
 
@@ -17,8 +18,10 @@ function App() {
   useEffect(() => {}, []);
 
   const search = (value) => {
-    setStartDate(value[0]._d);
-    setEndDate(value[1]._d);
+    if (value) {
+      setStartDate(value[0]._d);
+      setEndDate(value[1]._d);
+    }
   };
 
   const searchBooking = (city) => {
@@ -56,7 +59,7 @@ function App() {
             position: "absolute",
             display: "flex",
             height: "100vh",
-            backgroundSize: "contain",
+            backgroundSize: "cover",
             filter: "blur(3px)",
             justifyContent: "center",
             minWidth: "100%",
@@ -65,19 +68,32 @@ function App() {
               "url(https://scontent-rtl.akamaized.net/GED/09020000/9020600/9020609_700x0.webp)",
           }}
         />
-        <Card hoverable style={{ width: 500, borderRadius: 20, height: 700 }}>
+        <Card
+          hoverable
+          style={{
+            width: 800,
+            borderRadius: 4,
+            height: 700,
+          }}
+        >
           <h1>Travel.io</h1>
           <Space style={{ marginTop: 10 }} direction="vertical" size={18}>
             <RangePicker onChange={(value) => search(value)} />
           </Space>
           <Search
-            style={{ marginTop: 40 }}
+            style={{ marginTop: 40, marginBottom: 40 }}
             placeholder="Destination (exemple : Paris)"
             enterButton="Rechercher"
             size="large"
             onSearch={(value) => searchBooking(value)}
             loading={false}
           />
+          <Steps current={0}>
+            <Step title="Finished" description="Hotel" />
+            <Step title="In Progress" description="Vol" />
+            <Step title="Waiting" description="voiture" />
+            <Step title="Waiting" description="Payment" />
+          </Steps>
         </Card>
       </header>
     </div>
