641a64b75208c3c68419b8526995b6559571130e
iota <iota>
2020-12-18 16:39:10 +0100

    yolo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "antd/dist/antd.css";
-import moment from "moment";
-import { DatePicker, Space, Input, Card, Steps } from "antd";
+import { DatePicker, Space, Input, Card, Steps, List } from "antd";
+import { render } from "@testing-library/react";
 
 const { Search } = Input;
 const { Step } = Steps;
 
-const URLWSHOTEL = "https://wshotelapp.herokuapp.com/v1/booking";
-
 function App() {
-  const [dataJson, setDataJson] = useState();
+  const [hotels, setHotels] = useState([]);
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
-
+  const [steps, setSteps] = useState(0);
   useEffect(() => {}, []);
 
   const search = (value) => {
@@ -26,28 +23,69 @@ function App() {
 
   const searchBooking = (city) => {
     if (startDate && endDate && city) {
-      const url = `http://127.0.0.1:8080/v1/booking?city=${city}&startDate=${startDate}&endDate=${endDate}`;
-      fetch(url, {
+      searchHotel(city);
+    } else {
+      alert("Il manque une information");
+    }
+  };
+
+  const searchHotel = (city) => {
+    //Hotel WSHotel
+    fetch(
+      `http://127.0.0.1:8080/v1/booking?city=${city}&startDate=${startDate}&endDate=${endDate}`,
+      {
         mode: "cors",
         method: "GET",
         headers: {
           Accept: "*/*",
         },
-      })
-        .then((response) =>
-          response.json().then((data) => {
-            if (data) {
-              setDataJson(data);
-              console.log(data);
-            }
-          })
-        )
-        .catch((error) => {
-          console.log({ error });
-        });
-    } else {
-      alert("Il manque une information");
+      }
+    )
+      .then((response) =>
+        response.json().then((data) => {
+          if (data) {
+            console.log(data);
+            const listHotel = hotels;
+            listHotel.push({
+              compagny: "WS-Hotel",
+              ville: city,
+              idReservation: data.result.uidSejour,
+              price: data.result.price,
+            });
+            setHotels(listHotel);
+          }
+        })
+      )
+      .catch((error) => {
+        console.log({ error });
+      });
+
+    //Wati Hotel erreur 500 et cors
+    // fetch(`http://watihotelapi.azurewebsites.net/watiHotel/destinations`, {
+    //   method: "GET",
+    //   headers: {
+    //     Accept: "application/json",
+    //   },
+    // })
+    //   .then((response) =>
+    //     response.json().then((data) => {
+    //       if (data) {
+    //         console.log(data);
+    //       }
+    //     })
+    //   )
+    //   .catch((error) => {
+    //     console.log({ error });
+    //   });
+  };
+
+  const getStatusSteps = (position) => {
+    if (position === steps) {
+      return "En cours";
+    } else if (position < steps) {
+      return "Fini";
     }
+    return "En attente";
   };
 
   const { RangePicker } = DatePicker;
@@ -88,12 +126,28 @@ function App() {
             onSearch={(value) => searchBooking(value)}
             loading={false}
           />
-          <Steps current={0}>
-            <Step title="Finished" description="Hotel" />
-            <Step title="In Progress" description="Vol" />
-            <Step title="Waiting" description="voiture" />
-            <Step title="Waiting" description="Payment" />
+          <Steps current={steps}>
+            <Step title={getStatusSteps(0)} description="Hotel" />
+            <Step title={getStatusSteps(1)} description="Avion" />
+            <Step title={getStatusSteps(2)} description="Voiture" />
+            <Step title={getStatusSteps(3)} description="Payment" />
           </Steps>
+          {hotels && hotels.length > 0 && steps === 0 && (
+            <List
+              itemLayout="horizontal"
+              dataSource={hotels}
+              renderItem={(item) => (
+                <List.Item onClick={(item) => setSteps(steps + 1)}>
+                  <List.Item.Meta
+                    key={item.index}
+                    style={{ background: "#ddd" }}
+                    title={item.compagny}
+                    description={`${item.ville} - ${item.price}`}
+                  />
+                </List.Item>
+              )}
+            />
+          )}
         </Card>
       </header>
     </div>
